7698fb1f63125e2d9872f91ea908c766a5720f3b
jeevan4476 <jeevanr2431@gmail.com>
2025-04-17 02:02:13 +0530

    Added New Escrow

---
+++ programs/escrow/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("F8CYg2tDt59xQn5K9kbL3qCGbgo8YCvf3iQBvZcJwRuJ");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
